8ff5bd74cc708c7b1198c29cc44f826012193d0e
Agust Fricke <agustfricke@protonmail.com>
2023-02-16 14:10:20 -0300

    variables

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-console.log('Hola');
-window.alert('Este es un mensaje');
+// Variables: Es un contenedor que alacena datos
 
-// Este es un comentario
+// Como se crean?
+// 1- Se Declara(var, let, const)
+// 2- Asignacion
 
-/* 
-    Este 
-    es 
-    un comentario multi 
-    linea
-*/
+// let edad;
+// edad = 23;
+
+let edad = 23;
+edad = edad + 1; // Numerico
+
+let nombre = 'Tech con Agust';
+let nombre2 = "Tech con Agust"; // Strings
+
+// true & false
+let graduado = true;
+let is_admin = false; // Booleanos
+
+
+// console.log('Tengo', edad, 'años de edad')
+
+document.getElementById('nombre').innerHTML = 
+'Hola mi canal es ' + nombre;
+
+
+
+
+
+
