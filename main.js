d7fcf79f13162d4d803640a2fb1f9220457e069d
Agust Fricke <agustfricke@protonmail.com>
2023-02-16 19:11:01 -0300

    input del usuario

--- main.js
+++ main.js
@@ -1,35 +1,9 @@
-/*
+// El input del usuario
 
-    Expresiones aritméticas
-    Las Expresiones aritméticas son una combinacion de 
-    operandos (variables, valores, etc) y 
-    operadores (+ - * / % **)
+// let usuario = window.prompt('Ingresa tu nombre de usuario: ');
+// console.log('Tu nombre de usuario es:', usuario)
 
-*/
-
-let bananas = 10;
-
-// bananas = bananas + 5
-// bananas = bananas - 5
-// bananas = bananas * 2
-// bananas = bananas / 2
-// bananas = bananas % 3
-// bananas = bananas ** 3
-
-// Shortcut
-bananas += 5;
-console.log(bananas)
-
-
-/*
-
-En que orden se resuelven las operaciones?
-1- Parentesis ()
-2- Exponentes 
-3- Multiplicacion y divicion
-4- suma y resta
-
-*/
-
-let resultado = (2 + 5) * 2 + 5;
-console.log(resultado)
+document.getElementById('boton').onclick = function () {
+    usuario = document.getElementById('usuario').value;
+    console.log('Hola', usuario)
+}
