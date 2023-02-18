2fceb700f3713e2153872981c96a6bcaea8284b7
Agust Fricke <agustfricke@protonmail.com>
2023-02-18 16:52:20 -0300

    contador

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-// Math
+// Contador
 
-let a = 5.1;
+let contador = 0;
 
-// a = Math.round(a);
-// a = Math.floor(a);
-// a = Math.ceil(a);
-// a = Math.pow(a, 3);
+document.getElementById('sumar').onclick = function () {
+    contador += 1;
+    document.getElementById('contador').innerHTML = contador;
+}
 
+document.getElementById('restar').onclick = function () {
+    contador -= 1;
+    document.getElementById('contador').innerHTML = contador;
+}
 
-
-
-console.log(a)
+document.getElementById('reset').onclick = function () {
+    contador = 0;
+    document.getElementById('contador').innerHTML = contador;
+}
