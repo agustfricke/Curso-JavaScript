2fe62c478c6cfeb00208704e5ad8ac303a289b55
Agust Fricke <agustfricke@protonmail.com>
2023-02-18 02:51:27 -0300

    conversion de tipos

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-// El input del usuario
+// Conversion de tipos es cambiar el tipo de dato de un valor a otro
+// (booleanos, candenas de caracteres, numeros)
 
-// let usuario = window.prompt('Ingresa tu nombre de usuario: ');
-// console.log('Tu nombre de usuario es:', usuario)
+// let bananas = window.prompt('Ingrese la cantidad de bananas');
 
-document.getElementById('boton').onclick = function () {
-    usuario = document.getElementById('usuario').value;
-    console.log('Hola', usuario)
-}
+// console.log(typeof bananas);
+// bananas = Number(bananas);
+// console.log(typeof bananas);
+
+// bananas += 5;
+
+// console.log(bananas)
+
+let a;
+let b;
+let c;
+
+a = Number('5')
+b = String(5);
+c = Boolean(5);
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof c);
+
+
+
