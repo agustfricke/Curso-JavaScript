f790ba16493ce1ea7dce71c5821a6bcb7795725a
Agust Fricke <agustfricke@protonmail.com>
2023-02-18 16:43:14 -0300

    const

--- main.js
+++ main.js
@@ -1,27 +1,15 @@
-// Conversion de tipos es cambiar el tipo de dato de un valor a otro
-// (booleanos, candenas de caracteres, numeros)
+// Constantes
+// Son variables que no pueden cambiar su valor
 
-// let bananas = window.prompt('Ingrese la cantidad de bananas');
+const precio = 20
+let cantidad;
+let total;
 
-// console.log(typeof bananas);
-// bananas = Number(bananas);
-// console.log(typeof bananas);
-
-// bananas += 5;
-
-// console.log(bananas)
-
-let a;
-let b;
-let c;
-
-a = Number('5')
-b = String(5);
-c = Boolean(5);
-
-console.log(a, typeof a);
-console.log(b, typeof b);
-console.log(c, typeof c);
+cantidad = window.prompt('Ingrese la cantidad');
+cantidad = Number(cantidad);
 
 
 
+total = precio * cantidad;
+
+console.log(total);
