513d1c8af7a147fb3a039e4d42dd1511a38e435c
Agustin Fricke <agustfricke@protonmail.com>
2023-03-06 20:29:09 -0300

    04-sintaxis & 05-comentarios

---
+++ 04-sintaxis.js
@@ -0,0 +1,57 @@
+// La sintaxis de JavaScript
+// es una serie de reglas para que nustros programas funciones adecuadamente
+
+
+
+// literales
+10.55
+5
+
+'Hola'
+"Tech"
+
+// variables
+
+// Crear una variable 
+let a;
+var b;
+
+// Usar una variable
+a = 5;
+b = 5;
+let c = a + b; // 10
+
+// Como vemos las variables son usadas para almacenar valores 
+// podemos usar var let y const y ya mas adelante vernemos las diferencias
+// y luego suamos el igno igual para asignale el valor
+
+// Operadores
+
+// JavaScript usa Operadores aritmeticos para computar valores
+console.log((5 + 5) * 10);
+// Tambien lo podemos hacer con cadenas de caracteres 
+
+console.log('Hola' + ' ' + 'como' + ' ' + 'estas')
+
+// Permitidos para declarar variables o funciones -> deben comenzar con
+// A-Z a-z
+// $
+// _
+
+// Ten cuaidado con las mayusculas
+
+let hola;
+// No es lo mismo que 
+let Hola;
+
+// historicamente en JavaScript se escribe con camel case 
+
+let unNombreParaUnaVariable
+
+// Pero puedes usar pascal case o snake case
+
+let UnNombre; // pascal
+let hola_como_estas; //snake
+
+
+
